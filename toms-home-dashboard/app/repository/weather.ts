267f27249394cd5122245a41c8135aa71ce31cc1
tbuhl <tom@buhlnet.de>
2024-12-28 13:21:50 +0100

    Get token from config

--- toms-home-dashboard/app/repository/weather.ts
+++ toms-home-dashboard/app/repository/weather.ts
@@ -1,10 +1,12 @@
+import { getConfig } from "../common/ConfigHelper"
 import { Request } from "../common/Request"
 import { Weather } from "../models"
 
 export async function getCurrentWeather() {
   const response = await Request<Weather>(
-    "https://api.openweathermap.org/data/2.5/weather?lat=53.550341&lon=10.000654&units=metric&appid="
+    `https://api.openweathermap.org/data/2.5/weather?lat=53.550341&lon=10.000654&units=metric&appid=${
+      getConfig().OpenWeatherMapsAppId
+    }`
   )
-
   return response
 }
