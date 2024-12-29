56d345039a9aca6fba8d44f3d28b5e021dd54163
tbuhl <tom@buhlnet.de>
2024-12-29 15:30:32 +0100

    Allow cross origin

--- toms-home-dashboard/app/common/Request.ts
+++ toms-home-dashboard/app/common/Request.ts
@@ -1,5 +1,20 @@
-export async function Request<T>(url: string): Promise<T> {
-  const response = await (await fetch(url)).json()
+export async function Request<T>(
+  url: string,
+  crossOrigin?: boolean
+): Promise<T> {
+  let mode: RequestMode | undefined = undefined
+  let headers = {}
+
+  if (crossOrigin === true) {
+    mode = "cors"
+    headers = { "Access-Control-Allow-Origin": "http://192.168.178.60:3030" }
+  }
+
+  const result = await fetch(url, {
+    mode,
+    headers
+  })
+  const response = await result.json()
   const data: T = response
 
   return data
