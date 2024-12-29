213848d82c652b633206b3d5b6171bdd8ca3b7bd
tbuhl <tom@buhlnet.de>
2024-12-29 15:32:06 +0100

    Get appId from ConfigHelper

--- toms-home-dashboard/app/repository/weather.ts
+++ toms-home-dashboard/app/repository/weather.ts
@@ -3,10 +3,13 @@ import { Request } from "../common/Request"
 import { Weather } from "../models"
 
 export async function getCurrentWeather() {
+  const openweathermapsAppId = (await getConfig())?.OpenWeatherMapsAppId
+
+  if (!openweathermapsAppId) {
+    return
+  }
   const response = await Request<Weather>(
-    `https://api.openweathermap.org/data/2.5/weather?lat=53.550341&lon=10.000654&units=metric&appid=${
-      getConfig().OpenWeatherMapsAppId
-    }`
+    `https://api.openweathermap.org/data/2.5/weather?lat=53.550341&lon=10.000654&units=metric&appid=${openweathermapsAppId}`
   )
   return response
 }
